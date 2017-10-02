b84380b234d75eb9ade47f250127f7d5b34640dd
Florian Breitwieser <florian.bw@gmail.com>
2017-10-02 14:01:01 -0400

    update init.sh

--- tests/init.sh
+++ tests/init.sh
@@ -10,6 +10,7 @@ set -xeu
 ## Download taxonomy and genomic data into data/
 #$DIR/install/krakenu-download --db $DIR/data -R --include-viral-neighbors taxonomy refseq/archaea refseq/bacteria refseq/viral/Any
 #$DIR/install/krakenu-download --db $DIR/data --fna rna,genomic -R refseq/vertebrate_mammalian/Chromosome/taxid9606 
+$DIR/install/krakenu-download --db $DIR/data -R contaminants
 
 for i in viral viral-neighbors archaea bacteria; do 
   [[ -s "$DIR/data/all-$i.fna" ]] || find $DIR/data/library/$i -name '*.fna' -exec cat {} \; > $DIR/data/all-$i.fna
@@ -18,5 +19,5 @@ for i in viral viral-neighbors archaea bacteria; do
   [[ -s $DUSTED_F ]] || dustmasker -infmt fasta -in $DIR/data/all-$i.fna -level 20 -outfmt fasta | sed '/^>/! s/[^AGCT]/N/g' > "$DUSTED_F"
   mkdir -p $DIR/data/library/$i-dusted
   [[ -f "$DIR/data/library/$i-dusted/all-$i-dusted.fna" ]] || ln "$DUSTED_F" "$DIR/data/library/$i-dusted/all-$i-dusted.fna"
-  [[ -f "$DIR/data/library/$i-dusted/all-$i-dusted.fna.map" ]] || ln "$DIR/data/all-$i.map" "$DIR/data/library/$i-dusted/all-$i.map"
+  [[ -f "$DIR/data/library/$i-dusted/all-$i.map" ]] || ln "$DIR/data/all-$i.map" "$DIR/data/library/$i-dusted/all-$i.map"
 done
