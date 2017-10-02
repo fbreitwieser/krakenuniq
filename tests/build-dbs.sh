04fa4007500b2306799b6ed2bcc313714eb651b4
Florian Breitwieser <florian.bw@gmail.com>
2017-10-01 22:31:18 -0400

    Added build

--- tests/build-dbs.sh
+++ tests/build-dbs.sh
@@ -10,10 +10,10 @@ for K in 31 26 21; do
   #krakenu-build --kmer-len $K --minimizer-len 12 --threads 4 --db $DIR/dbs/refseq-viral-k$K --build --taxids-for-genomes --taxids-for-sequences --library-dir=$DIR/data/library/viral --taxonomy-dir=$DIR/data/taxonomy
 
   if [[ `uname` != "Darwin" ]]; then
-    #mkdir -p $DIR/dbs/refseq-bacteria-k$K
-    #krakenu-build --kmer-len $K --threads 4 --db $DIR/dbs/refseq-bacteria-k$K --build --taxids-for-genomes --taxids-for-sequences --library-dir=$DIR/data/library/bacteria --library-dir=$DIR/data/library/archaea --taxonomy-dir=$DIR/data/taxonomy
     mkdir -p $DIR/dbs/refseq-oct2017-k$K
-    krakenu-build --kmer-len $K --threads 4 --db $DIR/dbs/refseq-oct2017-k$K --build --taxids-for-genomes --library-dir=$DIR/data/library/viral-dusted --library-dir=$DIR/data/library/viral-neighbors-dusted --library-dir=$DIR/data/library/bacteria-dusted --library-dir=$DIR/data/library/archaea-dusted --library-dir=$DIR/data/libray/vertebrate_mammalia --taxonomy-dir=$DIR/data/taxonomy
+    krakenu-build --kmer-len $K --threads 20 --db $DIR/dbs/refseq-oct2017-k$K --build --taxids-for-genomes --taxids-for-sequences --library-dir=$DIR/data/library/viral-dusted --library-dir=$DIR/data/library/viral-neighbors-dusted --library-dir=$DIR/data/library/bacteria-dusted --library-dir=$DIR/data/library/archaea-dusted --library-dir=$DIR/data/library/vertebrate_mammalian --library-dir=$DIR/data/library/contaminants --taxonomy-dir=$DIR/data/taxonomy
+    mkdir -p $DIR/dbs/refseq-bacteria-k$K
+    krakenu-build --kmer-len $K --threads 20 --db $DIR/dbs/refseq-bacteria-k$K --build --taxids-for-genomes --taxids-for-sequences --library-dir=$DIR/data/library/bacteria --library-dir=$DIR/data/library/archaea --taxonomy-dir=$DIR/data/taxonomy
   fi
 done
 
