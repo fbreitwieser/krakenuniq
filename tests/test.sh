0509fd62df55059b185322e50324fa53c2eab4f0
Florian Breitwieser <florian.bw@gmail.com>
2019-02-18 15:00:45 -0500

    Run tests from any directory

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,7 @@
-../install_krakenuniq.sh `pwd`/install
-install/krakenuniq-download --db DB refseq/archaea/Chromosome taxonomy
+#!/bin/bash
+
+set -xeu
+
+`dirname $0`/../install_krakenuniq.sh `pwd`/install
+install/krakenuniq-download --dust --db DB refseq/archaea/Chromosome taxonomy
 install/krakenuniq-build --db DB --threads 2 --kmer-len 21 --minimizer-len 12 --taxids-for-genomes --taxids-for-sequences
