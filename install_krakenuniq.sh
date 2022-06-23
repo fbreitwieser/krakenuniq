e6f0ad04feb0ccc6305a7e296a68a868c99c98d4
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2022-06-23 11:57:57 -0400

    some cleanup

--- install_krakenuniq.sh
+++ install_krakenuniq.sh
@@ -73,14 +73,14 @@ export KRAKEN_DIR=$(perl -MCwd=abs_path -le 'print abs_path(shift)' "$1")
 
 mkdir -p "$KRAKEN_DIR"
 if [ "$INSTALL_JELLYFISH" == "1" ]; then
-echo "Installing jellyfish"
-(cd $KRAKEN_DIR
-if [ ! -e jellyfish-install/bin/jellyfish ]; then
-  wget https://github.com/gmarcais/Jellyfish/releases/download/v1.1.12/jellyfish-1.1.12.tar.gz 
-  tar xzf jellyfish-1.1.12.tar.gz && rm -rf jellyfish-1.1.12.tar.gz
-  mv jellyfish-1.1.12 jellyfish-install
-  cd jellyfish-install && ./configure --prefix=$PWD && make -j install || echo "Jellyfish 1 installation failed"
-fi)
+  (cd $KRAKEN_DIR
+  if [ ! -e jellyfish-install/bin/jellyfish ]; then
+    echo "Installing jellyfish"
+    wget https://github.com/gmarcais/Jellyfish/releases/download/v1.1.12/jellyfish-1.1.12.tar.gz 
+    tar xzf jellyfish-1.1.12.tar.gz && rm -rf jellyfish-1.1.12.tar.gz
+    mv jellyfish-1.1.12 jellyfish-install
+    cd jellyfish-install && ./configure --prefix=$PWD && make -j install || echo "Jellyfish 1 installation failed, building new database may not work properly"
+  fi)
 fi
 
 [[ "$ADD_DEBUG_INFO" == 1 ]] && MAKE_ARGS="$MAKE_ARGS NDEBUG=-g"
